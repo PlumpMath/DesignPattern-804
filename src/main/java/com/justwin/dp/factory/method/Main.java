788f18120a05fbae8fc30fe0dc97c6dd29ee05fb
justwin <zyqqq1024@163.com>
2016-03-13 22:18:48 +0800

    fix

--- src/main/java/com/justwin/dp/factory/method/Main.java
+++ src/main/java/com/justwin/dp/factory/method/Main.java
@@ -5,7 +5,7 @@ package com.justwin.dp.factory.method;
  * @author justwin
  * @date 2016年3月12日
  * @version 1.0
- * @description Factory method. 遵循"开放－封闭"原则，当需要扩展Operator类时 不需要修改factory类，将选择Operator的职能由Server端转移到client端
+ * @description Factory method.遵循"开放－封闭"原则，当需要扩展Operator类时 不需要修改factory类，将选择Operator的职能由Server端转移到client端
  */
 public class Main {
 
